cafc60d19064f545c3aca80719f34903ed59c487
Evgeniy Ratkov <e.ratkov@arenadata.io>
2026-04-14 13:57:09 +0300

    Revert "Add upgrade tests for new version 2.4"
    This reverts commit 7d858caa795061e42ed42a33b87860b159893a1a.

--- upgrade_test/sql/2.4_migrate_to_version_2.4.sql
+++
@@ -1,8 +0,0 @@
-\! gpconfig -c shared_preload_libraries -v 'diskquota-2.4.so' > /dev/null
-\! gpstop -raf > /dev/null
-
-\! gpconfig -s 'shared_preload_libraries'
-
-\c
-alter extension diskquota update to '2.4';
-\! sleep 5
